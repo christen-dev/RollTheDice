e44ddd18b43fc8fa17523e4b52f4d0e8f8c794d9
christensen-dev <dogsippepsi@gmail.com>
2024-01-17 10:37:24 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using RollTheDice;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string numOfRolls = "";
+
+        // Create new "RolledDice Object"
+        RollIt ri = new RollIt();
+
+        System.Console.WriteLine("How many times would you like to roll the dice?:");
+        numOfRolls = System.Console.ReadLine();
+
+        // Convert the string to an integer and assign it to a variable
+        int rolls = int.Parse(numOfRolls);
+
+        // Pass the parsed integer to the Roll method, and assign it to a new array
+        int[] resultArray = ri.Roll(rolls);
+
+        for (int i = 0; i < resultArray.Length; i++)
+        {
+            int percentage = resultArray[i] * 100 / rolls;
+
+            System.Console.Write($"{i + 2}:");
+
+            for (int j = 0; j < percentage; j++)
+            {
+                System.Console.Write("*");
+            }
+
+            System.Console.WriteLine($" ({percentage}%)");
+        }
+        System.Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+    }
+}
---
+++ RollTheDice.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RollTheDice;
+
+internal class RollIt
+{
+    // Create a Random object
+    Random random = new Random();
+
+    //Initialize Dice
+    int dice1 = 0;
+    int dice2 = 0;
+    int diceTotal = 0;
+
+    //Initalize Combination Array
+    int[] NumRolled = new int[11];
+
+
+    public int[] Roll(int numOfRolls)
+    {
+        for (int i = 0; i < numOfRolls; i++)
+        {
+            dice1 = random.Next(1, 7);
+            dice2 = random.Next(1, 7);
+            diceTotal = (dice1 + dice2);
+
+            NumRolled[(diceTotal - 2)]++;
+        }
+
+        return NumRolled;
+    }
+}
+
+
